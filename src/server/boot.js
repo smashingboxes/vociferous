29faff223fced9784bf786a7840726bf009f099e
Biko Tushinde <biko@smashingboxes.com>
2016-03-25 14:30:06 -0400

    bootstrapping all the node

--- src/server/boot.js
+++ src/server/boot.js
@@ -1,24 +1,7 @@
-
-var spawn = require('child_process').spawn;
-var execFile = require('child_process').execFile;
-var execFile = require('child_process').execFile;
 var server = require('./server.js');
-// var provider = require('./providers/presentations-provider.js')
-
-// var cmd1 = spawn('node', ['--version']);
-// cmd1.on('data', function(a,b){
-//   console.log(a,b);
-// });
-//
-// const child = execFile('node', ['./src/server/index.js'], (error, stdout, stderr) => {
-//   if (error) {
-//     throw error;
-//   }
-//   console.log(stdout);
-// });
-
 
 server.start()
 setTimeout(function(){
   require('./providers/presentations-provider.js');
+  require('../../client-server.js');
 }, 5000);
--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,7 @@
-var RedisCacheConnector   = require( 'deepstream.io-cache-redis' );
-var RedisMessageConnector = require( 'deepstream.io-msg-redis' );
-var Deepstream            = require( 'deepstream.io' );
-var PermissionHandler     = require('./permission-handler');
+var server = require('./server.js');
 
-var server = new Deepstream();
-
-server.set( 'cache', new RedisCacheConnector({
-  port: 6379,
-  host: 'localhost'
-}));
-
-server.set( 'messageConnector', new RedisMessageConnector({
-  port: 6379,
-  host: 'localhost'
-}));
-
-server.set('serverName', 'DeeplyStreaming');
-server.set('colors', true);
-server.set('tcpHost', '0.0.0.0');
-server.set('tcpPort', 6022);
-
-server.set( 'permissionHandler', new PermissionHandler() );
-
-server.start();
+server.start()
+setTimeout(function(){
+  require('./providers/presentations-provider.js');
+  require('../../client-server.js');
+}, 5000);
