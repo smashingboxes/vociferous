8d1abd91eae3bacc2c2a20d43ab904197fac8d35
Biko Tushinde <biko@smashingboxes.com>
2016-03-25 14:34:37 -0400

    port port port

--- src/client/components/Presentation/index.js
+++ src/client/components/Presentation/index.js
@@ -6,7 +6,7 @@ import deepstream from 'deepstream.io-client-js';
 // import DeepstreamMixin from 'deepstream.io-tools-react';
 
 // const client = deepstream( 'http://localhost:6020' );
-const client = deepstream( 'http://vox.biko.computer' );
+const client = deepstream( 'http://vox.biko.computer:6022' );
 
 // Components
 import ViewSlide from '../ViewSlide';
